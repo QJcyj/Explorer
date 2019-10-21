a13c20d65055f1506b2d86cc93c0e4cf8f522d86
qj <2467863212@qq.com>
2019-10-21 23:38:53 +0800

    初创工程

---
+++ RTE/_config/RTE_Components.h
@@ -0,0 +1,47 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Explorer' 
+ * Target:  'config' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+#define RTE_Driver_NAND_MemoryBus       /* Driver NAND Flash on Memory Bus */
+#define RTE_Drivers_CAN1                /* Driver CAN1 */
+        #define RTE_Drivers_CAN2                /* Driver CAN2 */
+        #define RTE_Drivers_CAN3                /* Driver CAN3 */
+#define RTE_Drivers_I2C1                /* Driver I2C1 */
+        #define RTE_Drivers_I2C2                /* Driver I2C2 */
+        #define RTE_Drivers_I2C3                /* Driver I2C3 */
+#define RTE_Drivers_I2C_MultiSlave      /* Driver I2C Multi-Slave */
+#define RTE_Drivers_SPI1                /* Driver SPI1 */
+        #define RTE_Drivers_SPI2                /* Driver SPI2 */
+        #define RTE_Drivers_SPI3                /* Driver SPI3 */
+        #define RTE_Drivers_SPI4                /* Driver SPI4 */
+        #define RTE_Drivers_SPI5                /* Driver SPI5 */
+        #define RTE_Drivers_SPI6                /* Driver SPI6 */
+#define RTE_Drivers_SPI_MultiSlave      /* Driver SPI Multi-Slave */
+#define RTE_Drivers_USART1              /* Driver USART1 */
+        #define RTE_Drivers_USART2              /* Driver USART2 */
+        #define RTE_Drivers_USART3              /* Driver USART3 */
+        #define RTE_Drivers_USART4              /* Driver USART4 */
+        #define RTE_Drivers_USART5              /* Driver USART5 */
+        #define RTE_Drivers_USART6              /* Driver USART6 */
+        #define RTE_Drivers_USART7              /* Driver USART7 */
+        #define RTE_Drivers_USART8              /* Driver USART8 */
+        #define RTE_Drivers_USART9              /* Driver USART9 */
+        #define RTE_Drivers_USART10             /* Driver USART10 */
+#define RTE_Drivers_USBD1               /* Driver USBD1 */
+#define RTE_Drivers_USBH1               /* Driver USBH1 */
+
+#endif /* RTE_COMPONENTS_H */
